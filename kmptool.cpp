f3ab6deb2cdfb573da9ee9fdd226fb4f7d57b504
Ermelber <ermelber@live.it>
2015-03-12 15:34:24 +0100

    ENPH, ITPT, ITPH Reading
    Have to tweak unknown values yet, though.

--- kmptool.cpp
+++ kmptool.cpp
@@ -150,7 +150,7 @@ void Write(const char *file, char *ext) {
 #define NICEHEX setw(2) << setfill('0')
 #define FCAST (unsigned)(unsigned char)
 
-		int numpt,pos,length,j;
+		int numpt,pos,length,j,a;
 		     for (j = 0; j < 18; j++)
              {
 			     pos = keiempi.find(Names[j],0)+8;
@@ -164,7 +164,6 @@ void Write(const char *file, char *ext) {
                         //TPNE
                         case 1:
                             numpt = length/24;
-                            //unsigned int x,y,z,scale;
                             out << "@ENPT" << endl << "#Enemy Points" << endl << "#Number of Points="<< numpt << endl;
                             out << "#ID:	X:	    	Y:	     	Z:	     	SCALE:	 	UNKNOWN1:	UNKNOWN2:" << endl;
                             for (int i = 0; i < numpt; i++)
@@ -182,20 +181,82 @@ void Write(const char *file, char *ext) {
                                 out << hex << uppercase << NICEHEX << FCAST keiempi[pos+(24*i)+16] << NICEHEX << FCAST keiempi[pos+(24*i)+17] << NICEHEX << FCAST keiempi[pos+(24*i)+18] << NICEHEX << FCAST keiempi[pos+(24*i)+19] << "\t";
                                 //unknown2
                                 out << hex << uppercase << NICEHEX << FCAST keiempi[pos+(24*i)+20] << NICEHEX << FCAST keiempi[pos+(24*i)+21] << NICEHEX << FCAST keiempi[pos+(24*i)+22] << NICEHEX <<  FCAST keiempi[pos+(24*i)+23];
-				out << endl << dec;
+				                out << endl << dec << setfill(' ');
                             }
                             break;
                         //HPNE
                         case 2:
-                            out << "@ENPH" << endl << "Enemy Points (Sections)" <<endl;
+                            numpt = length/72;
+                            out << "@ENPH" << endl << "#Enemy Points (Sections)" <<endl << "#Number of Sections="<< numpt << endl;
+                            out << "#ID:    START:	LENGTH:	PR1:	PR2:	PR3:	PR4:	PR5:	PR6:	PR7: 	PR8: 	PR9: 	PR10: 	PR11: 	PR12: 	PR13: 	PR14: 	PR15: 	PR16: 	NX1: 	NX2: 	NX3: 	NX4: 	NX5: 	NX6: 	NX7: 	NX8: 	NX9: 	NX10: 	NX11: 	NX12: 	NX13: 	NX14: 	NX15: 	NX16: 	UNKNOWN:" << endl;
+                            for (int i = 0; i < numpt; i++)
+                            {
+                                //ID
+                                out << i << "\t";
+                                //START
+                                a=0;
+                                out << (unsigned short)keiempi[pos+(72*i)+a]+((unsigned short)keiempi[pos+(72*i)+a+1]*256) << "\t";
+                                a+=2;
+                                //LENGTH
+                                out << setw(3)<< (unsigned short)keiempi[pos+(72*i)+a]+((unsigned short)keiempi[pos+(72*i)+a+1]*256) << "\t";
+                                //PREVIOUSNEXT
+                                for (a+=2;a<68;a+=2)
+                                {
+                                    if ((((uchar)keiempi[pos+(72*i)+a])&255==255) && (((uchar)keiempi[pos+(72*i)+a+1])&255==255))
+                                        out << setw(3)<< "-1" << "\t";
+                                    else
+                                        out << setw(3)<< (unsigned short)keiempi[pos+(72*i)+a]+((unsigned short)keiempi[pos+(72*i)+a+1]*256) << "\t";
+                                }
+                                //UNKNOWN
+                                out << endl ;
+                            }
                             break;
                         //TPTI
                         case 3:
-                            out << "@ITPT" << endl << "#Item Points" <<endl;
+                            numpt = length/20;
+                            out << "@ITPT" << endl << "#Item Routes" << endl << "#Number of Points="<< numpt << endl;
+                            out << "#ID:	X:	    	Y:	     	Z:	     	SCALE:	 	UNKNOWN:" << endl;
+                            for (int i = 0; i < numpt; i++)
+                            {
+                                out << i << "\t";
+                                //X
+                                out << left << setw(8) << bytesToFloat(keiempi[pos+(20*i)+3],keiempi[pos+(20*i)+2],keiempi[pos+(20*i)+1],keiempi[pos+(20*i)]) << "\t";
+                                //Y
+                                out << left << setw(8) << bytesToFloat(keiempi[pos+(20*i)+7],keiempi[pos+(20*i)+6],keiempi[pos+(20*i)+5],keiempi[pos+(20*i)+4])<< "\t";
+                                //Z
+                                out << left << setw(8) << bytesToFloat(keiempi[pos+(20*i)+11],keiempi[pos+(20*i)+10],keiempi[pos+(20*i)+9],keiempi[pos+(20*i)+8])<< "\t";
+                                //scale
+                                out << left << setw(8) << bytesToFloat(keiempi[pos+(20*i)+15],keiempi[pos+(20*i)+14],keiempi[pos+(20*i)+13],keiempi[pos+(20*i)+12])<< "\t";
+                                //unknown1
+                                out << hex << uppercase << NICEHEX << FCAST keiempi[pos+(20*i)+16] << NICEHEX << FCAST keiempi[pos+(20*i)+17] << NICEHEX << FCAST keiempi[pos+(20*i)+18] << NICEHEX << FCAST keiempi[pos+(20*i)+19];
+                                out << endl << dec << setfill(' ');
+                            }
                             break;
                         //HPTI
                         case 4:
-                            out << "@ITPH" << endl << "#Item Points (Sections)" <<endl;
+                            numpt = length/68;
+                            out << "@ITPH" << endl << "#Item Points (Sections)" <<endl << "#Number of Sections="<< numpt << endl;
+                            out << "#ID:    START:	LENGTH:	PR1:	PR2:	PR3:	PR4:	PR5:	PR6:	PR7: 	PR8: 	PR9: 	PR10: 	PR11: 	PR12: 	PR13: 	PR14: 	PR15: 	PR16: 	NX1: 	NX2: 	NX3: 	NX4: 	NX5: 	NX6: 	NX7: 	NX8: 	NX9: 	NX10: 	NX11: 	NX12: 	NX13: 	NX14: 	NX15: 	NX16:" << endl;
+                            for (int i = 0; i < numpt; i++)
+                            {
+                                //ID
+                                out << i << "\t";
+                                //START
+                                a=0;
+                                out << (unsigned short)keiempi[pos+(68*i)+a]+((unsigned short)keiempi[pos+(68*i)+a+1]*256) << "\t";
+                                a+=2;
+                                //LENGTH
+                                out << setw(3)<< (unsigned short)keiempi[pos+(68*i)+a]+((unsigned short)keiempi[pos+(68*i)+a+1]*256) << "\t";
+                                //PREVIOUSNEXT
+                                for (a+=2;a<68;a+=2)
+                                {
+                                    if ((((uchar)keiempi[pos+(68*i)+a])&255==255) && (((uchar)keiempi[pos+(68*i)+a+1])&255==255))
+                                        out << setw(3)<< "-1" << "\t";
+                                    else
+                                        out << setw(3)<< (unsigned short)keiempi[pos+(68*i)+a]+((unsigned short)keiempi[pos+(68*i)+a+1]*256) << "\t";
+                                }
+                                out << endl;
+                            }
                             break;
                         //TPKC
                         case 5:
